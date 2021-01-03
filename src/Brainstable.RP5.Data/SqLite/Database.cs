6f3aa69acd182dfd72ba062683df786760d6e730
Mozgovoy Sergey <brainstable@gmail.com>
2021-01-03 11:19:16 +0700

    Create static class Database for creating and deleting databases

---
+++ src/Brainstable.RP5.Data/SqLite/Database.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Data.SQLite;
+
+namespace Brainstable.RP5.Data.SqLite
+{
+    public static class Database
+    {
+        public static void CreateDatabase(string fileName, bool isOverWrite = false)
+        {
+            if (File.Exists(fileName))
+            {
+                if (isOverWrite)
+                {
+                    File.Delete(fileName);
+                    SQLiteConnection.CreateFile(fileName);
+                }
+            }
+            else
+            {
+                SQLiteConnection.CreateFile(fileName);
+            }
+        }
+
+        public static void DeleteDatabase(string fileName)
+        {
+            if (File.Exists(fileName))
+                File.Delete(fileName);
+        }
+    }
+}
